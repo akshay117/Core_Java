975b7d20fce24dde26236472d2ffc1a21c0a1565
Akshay V Anil <akshayvanil117@gmail.com>
2022-01-07 00:01:24 +0530

    Added Solution for generateDocument

---
+++ Daily-Grind/src/generateDocument/Solution1.java
@@ -0,0 +1,31 @@
+package generateDocument;
+
+import java.util.*;
+
+class Solution1 {
+
+  public boolean generateDocument(String characters, String document) {
+  for(int idx=0;idx<document.length();idx++){
+		char character = document.charAt(idx);
+		int documentFrequency   =countCharFrequency(character,document);
+		int charactersFrequency =	countCharFrequency(character,characters);
+		if(documentFrequency>charactersFrequency){
+			return false;
+		}
+	}
+		return true;
+
+}
+
+public int countCharFrequency(char character,String target){
+	int frequency = 0;
+	for(int idx=0;idx<target.length();idx++){
+		char charRec = target.charAt(idx);
+		if (charRec==character ){
+			frequency += 1;
+		}
+	}
+	return  frequency;
+}
+	
+}
